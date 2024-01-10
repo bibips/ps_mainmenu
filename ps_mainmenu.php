6dfc1da5f7b92a05603ff42118a4f305fcc6f3cb
Daniel Hlaváček <daniel.hlavacek@hotmail.cz>
2024-01-10 12:17:40 +0100

    Update ps_mainmenu.php

--- ps_mainmenu.php
+++ ps_mainmenu.php
@@ -69,7 +69,7 @@ class Ps_MainMenu extends Module implements WidgetInterface
     {
         $this->name = 'ps_mainmenu';
         $this->tab = 'front_office_features';
-        $this->version = '2.3.2';
+        $this->version = '2.3.3';
         $this->author = 'PrestaShop';
         $this->imageFiles = null;
 
